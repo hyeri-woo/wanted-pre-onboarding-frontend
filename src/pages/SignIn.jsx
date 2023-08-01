5900c5a54ab1eedde7a7cac193b245240a6b6a00
hyeri-woo <woohyeri0525@gmail.com>
2023-08-01 20:10:20 +0900

    ✨Feat: SignIn 페이지 UI 완성 (#2)

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { H1Style, ContainerStyle } from '../styles/CommonStyle';
+import Form from '../components/Form';
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <ContainerStyle>
+      <H1Style>SIGN IN</H1Style>
+      <Form btnText="로그인" emailWarning="" pwWarning="" />
+    </ContainerStyle>
+  );
 }
