23a243d59d8fa75fd9bfc29c09e82440ba1a8823
hyeri-woo <woohyeri0525@gmail.com>
2023-08-01 19:41:33 +0900

    ✨Feat: input 컴포넌트 완성

---
+++ src/components/Common/Input.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const InputStyle = styled.input`
+  background: none;
+  border-bottom: 1px solid var(--color-white70);
+  width: 95%;
+  padding: 15px 10px;
+  &::placeholder {
+    color: var(--color-white70);
+  }
+  &:hover {
+    border-bottom: 1px solid white;
+  }
+`;
+const WarningText = styled.p`
+  font-size: 0.7em;
+  text-align: start;
+  padding: 7px 20px;
+  height: 28px;
+`;
+
+export default function Input(props) {
+  const { type, inputId, labelText, warning, placeholder, dataTestId } = props;
+  return (
+    <>
+      <label className="a11y-hidden" htmlFor={inputId}>
+        {labelText}
+      </label>
+      <InputStyle
+        type={type ? type : 'text'}
+        id={inputId}
+        placeholder={placeholder}
+        data-testid={dataTestId}
+      />
+      <WarningText>{warning}</WarningText>
+    </>
+  );
+}
