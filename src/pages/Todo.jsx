ae8144eee81316c7e2c3480d4c3287d81016406f
hyeri-woo <woohyeri0525@gmail.com>
2023-08-01 21:23:35 +0900

    ✨Feat: Todo 페이지 UI 완성 ((#3)

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import { H1Style, ContainerStyle } from '../styles/CommonStyle';
+import TodoList from '../components/Todo/TodoList';
+import TodoForm from '../components/Todo/TodoForm';
+
+const Container = styled(ContainerStyle)`
+  position: relative;
+  max-width: 600px;
+  padding-bottom: 80px;
+`;
 
 export default function Todo() {
-  return <div>ToDo</div>;
+  return (
+    <Container>
+      <H1Style>TO DO</H1Style>
+      <TodoList />
+      <TodoForm />
+    </Container>
+  );
 }
