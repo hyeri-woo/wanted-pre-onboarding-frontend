ec256f3066ec55a494c2fc3a4a348381d09427a7
hyeri-woo <woohyeri0525@gmail.com>
2023-08-01 19:42:07 +0900

    💄Design: overflow hidden 추가

--- src/styles/CommonStyle.jsx
+++ src/styles/CommonStyle.jsx
@@ -10,6 +10,7 @@ const ContainerStyle = styled.div`
   border: 1px solid var(--color-white70);
   background: var(--color-white20);
   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
 `;
 
 export { ContainerStyle };
