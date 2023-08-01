950f5bf56df32a009a8fed9ab763910991c5dfe7
hyeri-woo <woohyeri0525@gmail.com>
2023-08-02 00:41:31 +0900

    📦Chore: redux 설치 및 세팅

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-// import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import store from './redux/index';
+import { Provider } from 'react-redux';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 );
