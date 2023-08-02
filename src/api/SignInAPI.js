f87384c7172439648119593d5ec734298a4e36b0
hyeri-woo <woohyeri0525@gmail.com>
2023-08-02 17:59:30 +0900

    🐛Fix: api.defaults.header 설정 삭제

--- src/api/SignInAPI.js
+++ src/api/SignInAPI.js
@@ -8,8 +8,6 @@ const SignInAPI = async (email, password) => {
     });
     if (response.status === 200) {
       window.localStorage.setItem('token', response.data.access_token);
-      api.defaults.headers.common['Authorization'] =
-        'Bearer ' + response.data.access_token;
       return true;
     }
   } catch (error) {
